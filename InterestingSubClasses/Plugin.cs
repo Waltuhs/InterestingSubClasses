bc54bba53cfad57118eae08c830651387467cc39
waltuh <162900253+Waltuhs@users.noreply.github.com>
2024-06-14 14:14:52 +0100

    Update Plugin.cs

--- InterestingSubClasses/Plugin.cs
+++ InterestingSubClasses/Plugin.cs
@@ -17,8 +17,8 @@ namespace InterestingSubClasses
         public override string Author => "sexy waltuh";
         public override string Name => "InterestingSubClasses";
         public override string Prefix => "ISC";
-        public override Version RequiredExiledVersion => new Version(8, 9, 4);
-        public override Version Version => new Version(2, 2, 0);
+        public override Version RequiredExiledVersion => new Version(8, 9, 5);
+        public override Version Version => new Version(2, 2, 2);
         public List<ISCRoleAPI> registeredRoles = new List<ISCRoleAPI>();
         public Dictionary<Player, string> customRoles = new Dictionary<Player, string>();
         public Dictionary<Player, CoroutineHandle> activeCoroutines = new Dictionary<Player, CoroutineHandle>();
